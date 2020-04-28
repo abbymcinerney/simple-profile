1174cd8b924ffca141b8c671ccaf231a6e01edff
Abby Mcinerney <abbymcinerney@Abbys-MacBook-Air.local>
2020-04-28 20:30:24 +0100

    final

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* SCRIPTS.JS */
 
 function changePic() {
-  document.getElementById("profilePic").src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian-prp.jpg";
+  document.getElementById("profilePic").src = "https://i.postimg.cc/nzs13p4q/IMG-5899.jpg";
 }
 
 function normalPic() {
-  document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
+  document.getElementById("profilePic") .src = "https://i.postimg.cc/C1CH8WvC/me.jpg";
 }
